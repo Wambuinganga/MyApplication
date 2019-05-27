3e0e838e8bd4b601842553335e60b7b32d68dfba
peshy <wambowmagola7@gmail.com>
2019-05-27 18:17:33 +0300

    This is my notebook app

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.myapplication;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tvhello;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvhello = findViewById(R.id.tvhello);
+        tvhello.setText("peshy ");
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+
+}
